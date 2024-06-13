a0d407493a2a36cf33926f20a875449297282774
Robert Stein <rdstein@caltech.edu>
2024-06-13 15:08:11 -0700

    Set name to winternlc

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,5 +1,5 @@
-from winter_corrections.example import test_nonlinearity, test_mask
-from winter_corrections.config import fits_file, cor_dir, save_dir, cutoff
+from winternlc.example import test_nonlinearity, test_mask
+from winternlc.config import fits_file, cor_dir, save_dir, cutoff
 
 if __name__ == "__main__":
     test_nonlinearity(fits_file, cor_dir, save_dir, cutoff)
