ec44cf248d07f4b2d55a281932e5cb5ccbe26693
Robert Stein <rdstein@caltech.edu>
2024-06-13 16:00:22 -0700

    Use pathlib for relative paths

--- winternlc/config.py
+++ winternlc/config.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # versions
 available_versions = ["v0.1" , "v1.0", "v1.1"]
 urls = ["https://zenodo.org/records/13905735", "https://zenodo.org/records/13863497", "https://zenodo.org/records/13905772"]
@@ -9,13 +10,18 @@ if zenodo_version in available_versions:
 else:
     raise ValueError(f"Version {zenodo_version} is not available. Please choose from {available_versions}")
 
+from pathlib import Path
+
+code_dir = Path(__file__).parent
+
+data_dir = code_dir / "data"
 
 # paths
-fits_file = '../data/example_data/example_science_image_mef.fits'
-save_dir = '../data/example_data/'
-cor_dir= '../data/linearity_corrections/'+zenodo_version
-test_directory = '/data/flats_iwr/20240610'
-output_directory = '../data/linearity_corrections/'+zenodo_version
+fits_file = data_dir / "example_data/example_science_image_mef.fits"
+save_dir = data_dir / "example_data"
+cor_dir = "/home/winter/GIT/winter_linearity/data/linearity_corrections" + zenodo_version
+test_directory = "/data/flats_iwr/20240610"
+output_directory = data_dir / "linearity_corrections" + zenodo_version
 
 # variables
 cutoff = 56000
