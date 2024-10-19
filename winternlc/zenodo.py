5983b86f9a635b6547a540f2982fdc18bf42e854
Robert Stein <rdstein@caltech.edu>
2024-10-18 17:01:48 -0700

    Actually do CI...

--- winternlc/zenodo.py
+++ winternlc/zenodo.py
@@ -9,8 +9,8 @@ CURRENT_ZENODO_VERSION = "v1.1"
 ZENODO_VERSION = os.getenv("WINTERNLC_VERSION", CURRENT_ZENODO_VERSION)
 
 _ZENODO_URL_MAP = {
-    "v0.1": "https://zenodo.org/record/13905735",
-    "v1.0": "https://zenodo.org/record/13863497",
+    "v0.1": "https://zenodo.org/api/records/13905735/files-archive",
+    "v1.0": "https://zenodo.org/api/records/13863497/files-archive",
     "v1.1": "https://zenodo.org/api/records/13905772/files-archive",
 }
 
