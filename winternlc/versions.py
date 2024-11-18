3a3baf8b72b74720c8801874bb0342baf4883e90
Robert Stein <rdstein@caltech.edu>
2024-11-18 12:06:51 -0500

    Fix versions

--- winternlc/versions.py
+++ winternlc/versions.py
@@ -15,5 +15,13 @@ def get_nlc_version(header: fits.header) -> str:
 
     :return: Version of the WinterNLC corrections
     """
+    # Choose the fallback version based on the BOARD_ID
+    fallback_version = LATEST_ZENODO_VERSION
+    board_id = header["BOARD_ID"]
+    if board_id == 4:
+        fallback_version = "v0.1"
 
-    return LATEST_ZENODO_VERSION
+    # Logic for choosing version based on dates/firmware etc
+    version = fallback_version
+
+    return version
