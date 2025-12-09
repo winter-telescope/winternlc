94b50082006b1b32304e204d28c5ecdc6d271a28
Robert David Stein <rdstein@umd.edu>
2025-12-09 13:14:25 -0500

    Default to latest version only (#15)

--- winternlc/__init__.py
+++ winternlc/__init__.py
@@ -2,3 +2,4 @@ from winternlc.apply import apply_mask_mef, apply_nlc_mef
 from winternlc.get_corrections import check_for_files
 from winternlc.mask import apply_mask
 from winternlc.non_linear_correction import apply_nlc_single
+from winternlc.zenodo import LATEST_ZENODO_VERSION
